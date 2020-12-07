3b5628cb46798b96d6a72153b46120a1174e5101
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-12-07 17:56:39 +0100

    [INIT][SETUP] * Component App was init

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:components/src/pages/home_temp.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Componentes',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Componentes'),
+        ),
+        body: HomePageTemp(),
+      ),
+    );
+  }
+}
