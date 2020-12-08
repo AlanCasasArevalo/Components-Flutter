a18dfb62081885f65e0e8f69c9ab44aba67d26e4
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-12-08 11:01:15 +0100

    [FEATURE][AVATAR] * Avatar page was changed to get an avatar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,6 +6,31 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: [
+          Container(
+            padding: EdgeInsets.all(5),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRMRxccQHwSsO7fbstR-6Da2s9r2vkVWnIKA&usqp=CAU'),
+              radius: 25,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.greenAccent,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage('https://larepublica.pe/resizer/gGcBSqmfhqzCGiZFE6j6i5AR5tk=/1250x735/top/smart/arc-anglerfish-arc2-prod-gruporepublica.s3.amazonaws.com/public/FM5VZUEPTRFM5FN44D4NWVBOOU.jpg'),
+          placeholder: AssetImage('assets/loading.gif'),
+          fadeInDuration: Duration(
+            milliseconds: 200
+          ),
+        ),
       ),
     );
   }
