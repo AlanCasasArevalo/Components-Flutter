133f1be97e09d579e529ff55cf188a9ff2970f3a
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-12-08 10:43:34 +0100

    [FEATURE][ALERT] * Alert page was configured

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -7,6 +7,15 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alert page'),
       ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Mostrar una alerta'),
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+          onPressed: () => _showAlert(context),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.accessible_forward_rounded),
         onPressed: () {
@@ -14,5 +23,41 @@ class AlertPage extends StatelessWidget {
         },
       ),
     );
+  }
+
+  void _showAlert(BuildContext context) {
+    showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16)
+            ),
+            title: Text('Titulo de la alerta'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text('Contenido de la caja que muestra una alerta de cualquier manera'),
+                FlutterLogo(
+                  size: 50,
+                )
+              ],
+            ),
+            actions: [
+              FlatButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: Text('Ok'),
+              ),
+              FlatButton(
+                onPressed: (){
+                  Navigator.of(context).pop();
+                },
+                child: Text('Cancelar'),
+              )
+            ],
+          );
+        }
+        );
   }
 }
