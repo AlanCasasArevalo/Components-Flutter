ba8b9158f0df70982ca253e05513df535c8d0a74
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-12-08 10:05:48 +0100

    [FEATURE][CARD] * New Image Card was added to our Cards list * Assets was added

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -9,9 +9,7 @@ class CardPage extends StatelessWidget {
       ),
       body: ListView(
         padding: EdgeInsets.all(8),
-        children: [
-          _bigCardType()
-        ],
+        children: [_bigCardType(), SizedBox(height: 16.0), _imageCardType()],
       ),
     );
   }
@@ -24,15 +22,43 @@ Widget _bigCardType() {
         ListTile(
           leading: Icon(Icons.add_a_photo_outlined, color: Colors.blue),
           title: Text('Titulo de la tarjeta'),
-          subtitle: Text('Esto es un titulo largo para poder hacer muy muy diferente que es un titulo muy grande lorem ipsum in'),
+          subtitle: Text(
+              'Esto es un titulo largo para poder hacer muy muy diferente que es un titulo muy grande lorem ipsum in'),
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
             FlatButton(onPressed: () {}, child: Text('Cancelar')),
-            FlatButton(onPressed: () {}, child: Text('Ok'),)
+            FlatButton(
+              onPressed: () {},
+              child: Text('Ok'),
+            )
           ],
         )
+      ],
+    ),
+  );
+}
+
+Widget _imageCardType() {
+  return Card(
+    child: Column(
+      children: [
+        FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'),
+          image: NetworkImage(
+              'https://photographylife.com/wp-content/uploads/2010/11/Maroon-Bells-with-Snow.jpg'),
+          fadeInDuration: Duration(milliseconds: 200),
+          fit: BoxFit.cover,
+          height: 250,
+        ),
+        // Image(
+        //     image: NetworkImage(
+        //         'https://photographylife.com/wp-content/uploads/2010/11/Maroon-Bells-with-Snow.jpg')
+        // ),
+        Container(
+            padding: EdgeInsets.all(8),
+            child: Text('Habria que poner un texto dummy'))
       ],
     ),
   );
