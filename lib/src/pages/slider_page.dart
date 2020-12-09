41ac084b7f1c5b14a26b0a46aa4d96b5302f53fe
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-12-09 17:23:49 +0100

    [FEATURE][SLIDER] * Switch and checkbox examples were implemented

--- lib/src/pages/slider_page.dart
+++ lib/src/pages/slider_page.dart
@@ -7,6 +7,7 @@ class SliderPage extends StatefulWidget {
 
 class _SliderPageState extends State<SliderPage> {
   double _sliderValue = 100;
+  bool _blockSlider = true;
 
   @override
   Widget build(BuildContext context) {
@@ -19,8 +20,9 @@ class _SliderPageState extends State<SliderPage> {
         child: Column(
           children: [
             _sliderCreation(),
+            _checkBoxCreation(),
+            _switchCreation(),
             Expanded(child: _imageCreation())
-
           ],
         ),
       ),
@@ -29,13 +31,13 @@ class _SliderPageState extends State<SliderPage> {
 
   Widget _sliderCreation() {
     return Slider(
-      activeColor: Colors.indigoAccent,
+        activeColor: Colors.indigoAccent,
         label: 'Tamaño de la imagen',
         // divisions: 100,
         min: 0,
         max: 400,
         value: _sliderValue,
-        onChanged: (value) {
+        onChanged: ( !_blockSlider ) ? null : (value) {
           setState(() {
             _sliderValue = value;
           });
@@ -46,11 +48,43 @@ class _SliderPageState extends State<SliderPage> {
     return FadeInImage(
       width: _sliderValue,
       fit: BoxFit.contain,
-      image: NetworkImage('https://lanetaneta.com/wp-content/uploads/2019/09/%C2%BFC%C3%B3mo-consigui%C3%B3-Wolverine-su-esqueleto-de-Adamantium-y-sus-garras.jpg'),
+      image: NetworkImage(
+          'https://lanetaneta.com/wp-content/uploads/2019/09/%C2%BFC%C3%B3mo-consigui%C3%B3-Wolverine-su-esqueleto-de-Adamantium-y-sus-garras.jpg'),
       placeholder: AssetImage('assets/loading.gif'),
       fadeInDuration: Duration(
           milliseconds: 200
       ),
     );
+  }
+
+  Widget _checkBoxCreation() {
+    // return Checkbox(
+    //     value: _blockSlider,
+    //     onChanged: (value) {
+    //       setState(() {
+    //         _blockSlider = value;
+    //       });
+    //     });
+
+    return CheckboxListTile(
+        title: Text('Bloquear Slider'),
+        value: _blockSlider,
+        onChanged: (value) {
+          setState(() {
+            _blockSlider = value;
+          });
+        });
+  }
+
+  Widget _switchCreation() {
+
+    return SwitchListTile(
+        title: Text('Bloquear Slider'),
+        value: _blockSlider,
+        onChanged: (value) {
+          setState(() {
+            _blockSlider = value;
+          });
+        });
   }
 }
