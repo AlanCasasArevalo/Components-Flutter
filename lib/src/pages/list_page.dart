46cf219333348d2aa433a832fcc7982ef5a7e499
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-12-09 17:59:01 +0100

    [FEATURE][LIST] * List of images was implemented

--- lib/src/pages/list_page.dart
+++ lib/src/pages/list_page.dart
@@ -6,8 +6,21 @@ class ListPage extends StatefulWidget {
 }
 
 class _ListPageState extends State<ListPage> {
+  ScrollController _scrollController = new ScrollController();
 
-  List<int> _numberList = [0, 1, 2, 3, 4];
+  List<int> _numberList = new List();
+  int _lastItem = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _addTenNewElements();
+    _scrollController.addListener(() {
+      if(_scrollController.position.pixels == _scrollController.position.maxScrollExtent) {
+        _addTenNewElements();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,17 +34,24 @@ class _ListPageState extends State<ListPage> {
 
   Widget _listCreation() {
     return ListView.builder(
+        controller: _scrollController,
         itemCount: _numberList.length,
         itemBuilder: (BuildContext context, int index) {
           final image = _numberList[index];
           return FadeInImage(
             image: NetworkImage('https://picsum.photos/500/300?random=$image'),
             placeholder: AssetImage('assets/loading.gif'),
-            fadeInDuration: Duration(
-                milliseconds: 200
-            ),
+            fadeInDuration: Duration(milliseconds: 200),
           );
-        }
-    );
+        });
+  }
+
+  _addTenNewElements() {
+    for (var i = 1; i < 10; i++) {
+      _lastItem++;
+      _numberList.add(_lastItem);
+    }
+
+    setState(() {});
   }
 }
